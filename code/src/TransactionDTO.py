5d8c180d473c0dd977875ef55b618fcb01e44c62
jayant1628 <82391690+jayant1628@users.noreply.github.com>
2025-03-26 16:53:19 +0530

    Create TransactionDTO.py for proper output structure

---
+++ code/src/TransactionDTO.py
@@ -0,0 +1,17 @@
+class TransactionDTO:
+               transactionID: str
+               extractedEntities: list[str]
+               entityType: list[str]
+               riskScore: float
+               supportingEvidence: list[str]
+               confidenceScore: float
+               reason: str
+
+               def __init__(self, transactionID, extractedEntities, entityType, riskScore, supportingEvidence, confidenceScore, reason):
+                              self.transactionID = transactionID
+                              self.extractedEntities = extractedEntities
+                              self.entityType = entityType
+                              self.riskScore = riskScore
+                              self.supportingEvidence = supportingEvidence
+                              self.confidenceScore = confidenceScore
+                              self.reason = reason
